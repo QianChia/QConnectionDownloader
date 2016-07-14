1de7a34337eeb8e332995ee4a4f8b76ca36fc39b
仟 <jhqian0228@icloud.com>
2016-07-15 02:44:07 +0800

    chore: 更新测试方法

--- QConnectionDownloaderExample/ViewController.m
+++ QConnectionDownloaderExample/ViewController.m
@@ -12,23 +12,24 @@
 
 @interface ViewController ()
 
-@property (nonatomic, strong) NSURL *url;
-@property (nonatomic, strong) UIButton *btn;
+@property (nonatomic, strong) UIButton *progressBtn1;
+@property (nonatomic, strong) UIButton *progressBtn2;
+
+@property (nonatomic, strong) NSURL *url1;
+@property (nonatomic, strong) NSURL *url2;
 
 @end
 
 @implementation ViewController
 
-- (void)downloadFile:(UIButton *)sender {
-    
-    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_01.mp4"];
-    self.url = url;
+/// 开始下载
+
+- (void)startDownloadWithURL:(NSURL *)url button:(UIButton *)button {
     
     [[QDownloaderManager sharedManager] q_downloadWithURL:url progress:^(float progress) {
         
         dispatch_async(dispatch_get_main_queue(), ^{
-            
-            [sender q_setButtonWithProgress:progress lineWidth:5 lineColor:nil backgroundColor:[UIColor yellowColor]];
+            [button q_setButtonWithProgress:progress lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
         });
         
     } successed:^(NSString *targetPath) {
@@ -41,25 +42,74 @@
     }];
 }
 
-- (IBAction)startDownloadFile:(UIButton *)sender {
+/// 暂停下载
+
+- (void)pauseDownloadWithURL:(NSURL *)url {
     
-    self.btn = sender;
-    [self downloadFile:sender];
+    [[QDownloaderManager sharedManager] q_pauseWithURL:url];
 }
 
-- (IBAction)goonDownloadFile:(UIButton *)sender {
+/// 取消下载
+
+- (void)cancelDownloadWithURL:(NSURL *)url button:(UIButton *)button {
     
-    [self downloadFile:self.btn];
+    [[QDownloaderManager sharedManager] q_cancelWithURL:url];
+    
+    [button q_setButtonWithProgress:0 lineWidth:10 lineColor:nil backgroundColor:[UIColor yellowColor]];
 }
 
-- (IBAction)pauseDownloadFile:(UIButton *)sender {
+/******************************************************************************************************************************/
+
+- (IBAction)start1:(UIButton *)sender {
     
-    [[QDownloaderManager sharedManager] q_pauseWithURL:self.url];
+    NSURL *url = [NSURL URLWithString:@"http://dldir1.qq.com/qqfile/QQforMac/QQ_V4.0.2.dmg"];
+    
+    self.url1 = url;
+    self.progressBtn1 = sender;
+    
+    [self startDownloadWithURL:url button:sender];
 }
 
-- (IBAction)cancelDownloadFile:(UIButton *)sender {
+- (IBAction)goon1 {
     
-    [[QDownloaderManager sharedManager] q_cancelWithURL:self.url];
+    [self startDownloadWithURL:self.url1 button:self.progressBtn1];
+}
+
+- (IBAction)pause1 {
+    
+    [self pauseDownloadWithURL:self.url1];
+}
+
+- (IBAction)cancel1 {
+    
+    [self cancelDownloadWithURL:self.url1 button:self.progressBtn1];
+}
+
+/******************************************************************************************************************************/
+
+- (IBAction)start2:(UIButton *)sender {
+    
+    NSURL *url = [NSURL URLWithString:@"http://120.25.226.186:32812/resources/videos/minion_01.mp4"];
+    
+    self.url2 = url;
+    self.progressBtn2 = sender;
+    
+    [self startDownloadWithURL:url button:sender];
+}
+
+- (IBAction)goon2 {
+    
+    [self startDownloadWithURL:self.url2 button:self.progressBtn2];
+}
+
+- (IBAction)pause2 {
+    
+    [self pauseDownloadWithURL:self.url2];
+}
+
+- (IBAction)cancel2 {
+    
+    [self cancelDownloadWithURL:self.url2 button:self.progressBtn2];
 }
 
 @end
